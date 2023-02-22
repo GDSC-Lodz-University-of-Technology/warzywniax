9b16ff510aa71ba36f6197b74fff937d38d84e48
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2023-02-22 01:23:04 +0100

    chore(project-setup): add git-hooks
    Related work item: GH-1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './index.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
