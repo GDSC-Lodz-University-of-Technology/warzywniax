cd3915c9f8f7b546e01e2f6e48b2ecf89393259f
Michael Kidawa <mkidawa1@gmail.com>
2023-03-17 02:28:45 +0100

    chore(project-setup): additional mui config (#22)

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import './locales/i18n';
 import './index.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { customTheme } from './configs/mui.config';
+import { customTheme } from './mui/mui.config';
 import { router } from './router/router';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
 });
